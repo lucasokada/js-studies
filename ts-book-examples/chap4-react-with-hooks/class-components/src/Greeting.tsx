d675e4355e47032890e34c448e39b376ec2711da
leiki <leiki@ciandt.com>
2022-05-31 16:53:38 -0300

    hooks

--- ts-book-examples/chap4-react-with-hooks/class-components/src/Greeting.tsx
+++ ts-book-examples/chap4-react-with-hooks/class-components/src/Greeting.tsx
@@ -9,19 +9,41 @@ interface GreetingState {
     message: string
 }
 
-export default class Greeting extends React.Component<GreetingProps, GreetingState> {
-    constructor(props: GreetingProps) {
-        super(props)
+export default class Greeting extends React.Component<GreetingProps> {
+    constructor(props: GreetingProps){
+        super(props);
         this.state = {
-            message: `Hello from, ${props.name}`
+            message: Greeting.getNewMessage(props.name)
         }
     }
 
-    render() {
-        if(!this.props.name) {
-            return <div>no name given</div>
+    state: GreetingState;
+
+    static getDerivedStateFromProps(props: GreetingProps, state:GreetingState) {
+        console.log(props, state);
+        if(props.name && props.name !== state.message) {
+            const newState = {...state};
+            newState.message =
+            Greeting.getNewMessage(props.name)
+            
+            return newState;
         }
 
-        return <div>{this.state.message}</div>
+        return state;
+    
+    }
+    static getNewMessage(name: string = "") {
+        return `Hello from, ${name}`;
+    }
+
+    render() {
+        console.log("rendering Greeting")
+        if(!this.props.name) {
+            return <div>no name given</div>;
+        }
+        
+        return <div>
+            {this.state.message}
+        </div>;
     }
 }
